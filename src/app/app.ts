1ff0d7f05e169226c90004440eda728fb060270e
LIU YAO <liuyao1088@163.com>
2026-02-25 17:55:57 +0100

    Improve Footer: layout, links, and functionality

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,12 +4,14 @@ import { Navbar } from './navbar/navbar';
 import { Home } from './home/home';
 import { MoviesList } from './movies-list/movies-list';
 import { Nav } from './nav/nav';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Navbar,Home,MoviesList,Nav],
+  standalone: true,
+  imports: [RouterOutlet, Navbar, Home, MoviesList, Nav, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrls: ['./app.scss']
 })
 export class App {
   protected readonly title = signal('ProjetTD1');
