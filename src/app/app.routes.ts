c5f60792906e0647198d8f2049775169bf0ef307
ruofan <email>
2026-03-02 20:21:57 +0100

    watchlist et panier pour user

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,14 @@ import { Home } from './home/home';
 import { MoviesList } from './movies-list/movies-list';
 import { AddMovie } from './add-movie/add-movie';
 import { EspacePerso } from './espace-perso/espace-perso';
+import { MonPanier } from './mon-panier/mon-panier';
+
 
 export const routes: Routes = [
     { path: '', component: Home },
     { path: 'movies', component: MoviesList },
     { path: 'add-movie', component: AddMovie },
-    { path: 'espace-perso', component: EspacePerso }
+    { path: 'espace-perso', component: EspacePerso },
+    { path: 'mon-panier', component: MonPanier },
+    
 ];
