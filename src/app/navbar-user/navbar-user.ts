1aa20687ebe2b519d9d1d8265ff321f0edd416ef
ruofan <email>
2026-03-04 10:42:35 +0100

    reussi fonction search

--- src/app/navbar-user/navbar-user.ts
+++ src/app/navbar-user/navbar-user.ts
@@ -1,6 +1,7 @@
-import { Component, Input, Output, EventEmitter, NgModule } from '@angular/core';
+import { Component, Input, Output, EventEmitter, NgModule, inject } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { RouterLink, Router } from '@angular/router';
+import { Search } from '../services/search';
 
 @Component({
   selector: 'app-navbar-user',
@@ -23,5 +24,10 @@ export class NavbarUser {
     this.router.navigate(['/']);
   }
 
-  searchText: string = '';
+  private readonly search = inject(Search);
+
+  onSearch(value: string) {
+  this.search.query.set(value);
+}
+
 }
---
+++ src/app/services/search.ts
@@ -0,0 +1,10 @@
+import { Injectable, signal } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class Search {
+  query = signal('');
+  
+}
